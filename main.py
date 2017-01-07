f97dfb3ace6de0d628b1fe9aa7c7324e0e5eee6f
Fabio Rosada <r.fabio95@gmail.com>
2017-01-07 17:47:47 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import theverge_downloader as tvd
+import item_reader as ir
+import analyzer as an
+
+if __name__ == '__main__':
+    print "\n### main.py ###\n"
+
+    ''' ADD FUNCTION CALLS HERE '''
+
+    print "\n"
