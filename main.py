c02965ec4445ad6ad67d1d78ce2c1b2113d61312
Fabio Rosada <r.fabio95@gmail.com>
2017-01-16 17:31:03 +0100

    itemsets done (very slow with json)

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Password for ssh user lWMMC7uUGUHd
-
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 import re
@@ -7,13 +5,18 @@ import re
 small_file = '/srv/sample.json'
 large_file = '/srv/2015-01-08_geo_en_it_10M.plain.json'
 
+def parseText(tweet):
+    #TO BE IMPLEMENTED
+    return tweet[23]
+
 if __name__ == '__main__':
     print "\n### main.py ###\n"
 
     spark_session = SparkSession.builder.appName("PythonSON").getOrCreate()
 
-    tweets = spark_session.read.json(small_file)
-    print tweets[1]
+    tweets = spark_session.read.json(small_file).rdd.map(lambda x: parseText(x))
 
+    #print tweets.take(10)
+    
 
     print "\n"
