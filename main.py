22f7cabdacabf1a7e54960d477e92aa2052f89ba
Fabio Rosada <r.fabio95@gmail.com>
2017-01-16 19:39:30 +0100

    blablabla

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from pyspark import SparkContext
 from pyspark.sql import SparkSession
 import re
 
 small_file = '/srv/sample.json'
 large_file = '/srv/2015-01-08_geo_en_it_10M.plain.json'
 
-def parseText(tweet):
-    #TO BE IMPLEMENTED
-    return tweet[23]
+#TO BE IMPLEMENTED
+def parse_text(tweet):
+    #sistemare con i simboli
+    #return tweet[23].split()
+    #return text param from tweet
+    return tweet[23].split()
+
+def print_dict(mydict):
+    boh = sorted(mydict, key=mydict.get, reverse=True)
+    print "\n\tTOP 30 ABSOLUTE\n"
+    count = 1
+    for x in boh[0:30]:
+       print str(count) + "\t" + x + ": " + str(mydict[x])
+       count +=1
+
+    print "\n"
 
 if __name__ == '__main__':
     print "\n### main.py ###\n"
 
+    #create spark context
     spark_session = SparkSession.builder.appName("PythonSON").getOrCreate()
 
-    tweets = spark_session.read.json(small_file).rdd.map(lambda x: parseText(x))
+    #read tweets file (sample or complete) and parse text from every tweet
+    #maybe with parallelize e lista
+    tweets = spark_session.read.json(small_file).rdd.flatMap(lambda x: parse_text(x)).countByValue()
 
-    #print tweets.take(10)
-    
+    #print tweets
 
-    print "\n"
+    print_dict(tweets)
