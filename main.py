26bc83ffa9f14847c63d2d6a11ff80ece93ba1f8
Rosada Fabio <frosada@lab050304.dsi.unive.it>
2017-01-17 14:27:21 +0100

    a-priori couples done

--- main.py
+++ main.py
@@ -5,22 +5,40 @@ import re
 small_file = '/srv/sample.json'
 large_file = '/srv/2015-01-08_geo_en_it_10M.plain.json'
 
-threshold = 3
+threshold = 10
+
+
+
+def text_cleaner(text):
+    text = re.sub('[^A-Za-z0-9@_]+|http.*|RT', '', text.lower())
+    return text
 
 #TO BE IMPLEMENTED
 def parse_text_json(tweet):
     #return text param from tweet
-    return tweet[23].split()
+    res = []
+    for t in tweet[23].split():
+        t = text_cleaner(t)
+        if not t=='':
+                res.append(t)
+    return res
 
 def print_dict(mydict):
     boh = sorted(mydict, key=mydict.get, reverse=True)
     print "\n\tTOP 30 ABSOLUTE\n"
     count = 1
     for x in boh[0:30]:
-       print str(count) + "\t" + x + ": " + str(mydict[x])
+       print str(count) + "\t" + str(x) + ": " + str(mydict[x])
        count +=1
 
     print "\n"
+
+
+def alphabet_ordered(a,b):
+        if a<b:
+                return (a,b)
+        else:
+                return (b,a)
 
 def nested_loop(tweet):
     resultlist = []
@@ -30,11 +48,16 @@ def nested_loop(tweet):
         y = x+1
         if text[x] in tweets:
                 while y < len(text):
-                        if text[y] in tweets:
-                                resultlist.append((text[x],text[y]))
+                        if text[y] in tweets and not text[y]==text[x]:
+                                resultlist.append(alphabet_ordered(text[x],text[y]))
                         y += 1
         x += 1
     return resultlist
+
+def filter_dict(my_dict):
+    for key, value in my_dict.items():
+        if value < threshold:
+            del my_dict[key]
 
 if __name__ == '__main__':
     print "\n### main.py ###\n"
@@ -45,13 +68,16 @@ if __name__ == '__main__':
     #read tweets file (sample or complete) and parse text from every tweet
     tweets = spark_session.read.json(small_file).rdd.flatMap(lambda x: parse_text_json(x)).countByValue()
 
-    for key, value in tweets.items():
-        if value < threshold:
-            del tweets[key]
+    filter_dict(tweets)
+    #tweets.filter(lambda x: tweets[x]>threshold)
 
     #print tweets
 
-    bucket = spark_session.read.json(small_file).rdd.flatMap(lambda x: nested_loop(x))
+    bucket = spark_session.read.json(small_file).rdd.flatMap(lambda x: nested_loop(x)).countByValue()
 
-    print bucket.collect()
+    filter_dict(bucket)
+
+    #print bucket
+    print_dict(bucket)
+
 
